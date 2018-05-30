5856162aececcc4a6ce4d6e0d743ba0b2dd7e601
saxgutierrez <diegofdog@gmail.com>
2018-05-30 18:55:41 -0500

    agregando importaciones

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import 'raf/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
 import App from './App';
 import './index.css';
 
